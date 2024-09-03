cd2c12859cb491a5b76ea393fc0bb4eddf36529c
Angello Pomayay <angello.pomayay@outlook.com>
2024-09-03 18:50:18 +0200

    update

--- js_scritps/scroll_and_carousel.js
+++ js_scritps/scroll_and_carousel.js
@@ -14,13 +14,23 @@ $(function() {
 });
 
 $(document).ready(function() {
-    $('.carousel').slick({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-        accessibility: false,
-        pauseOnHover: false,
+    function setCarouselSettings() {
+        var slidesToShow = $(window).width() <= 768 ? 3 : 5;
+        $('.carousel').slick({
+            slidesToShow: slidesToShow,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows: false,
+            accessibility: false,
+            pauseOnHover: false,
+        });
+    }
+
+    setCarouselSettings();
+
+    $(window).resize(function() {
+        $('.carousel').slick('unslick'); // Destroy the current instance
+        setCarouselSettings(); // Reinitialize with new settings
     });
 });
